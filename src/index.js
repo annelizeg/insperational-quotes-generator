2505513e738de7f740ae743f30cd7dcfa077cec4
Annelize Grobbelaar <91416875+annelizeg@users.noreply.github.com>
2025-02-26 13:25:17 +1030

    Added basic form handling and Typwriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  let quoteElement =
+    " It is often the small steps, not the giant leaps, that bring about the most lasting change.";
+
+  new Typewriter("#quote", {
+    strings: quoteElement,
+    autoStart: true,
+    delay: 30,
+  });
+
+  //   console.log(quoteElement);
+}
+
+let quoteFormElement = document.querySelector("#quote-generating-form");
+quoteFormElement.addEventListener("submit", generateQuote);
