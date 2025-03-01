fcfc7f6eaddaf47a4b3d92924d1bad7413f153e5
Annelize Grobbelaar <91416875+annelizeg@users.noreply.github.com>
2025-03-02 09:40:36 +1030

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
+function displayquote(response) {
+  console.log("quote generated");
+
+  new Typewriter("#quote", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 30,
+    cursor: "",
+  });
+}
+
 function generateQuote(event) {
   event.preventDefault();
 
-  let quoteElement =
-    " It is often the small steps, not the giant leaps, that bring about the most lasting change.";
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "feb0504864ab3c8o978403c9t3b099b5";
+  let context =
+    "You are a very positive, optimist and insperational AI assistent. You like to generate short 4 line insperational qoutes in basic HTML, separate each line with an <br/>. Make sure to follow the user instructions. Only provide the quote without stating that it is in HTML. Right at the bottom, sign the quote with '- Shecodes AI' inside a <strong> and <em> element.";
+  let prompt = `User instructions: Please generate an insperational quote about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  new Typewriter("#quote", {
-    strings: quoteElement,
-    autoStart: true,
-    delay: 30,
-  });
+  console.log("generating quote......");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayquote);
 
   //   console.log(quoteElement);
 }
