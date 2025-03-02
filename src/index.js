6bcd95277f1c576564dcacf46616432f5211f713
Annelize Grobbelaar <91416875+annelizeg@users.noreply.github.com>
2025-03-02 21:26:13 +1030

    Updated hourglass image, added pointer

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ function generateQuote(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "feb0504864ab3c8o978403c9t3b099b5";
   let context =
-    "You are a very positive, optimist and insperational AI assistent. You like to generate short 4 line insperational qoutes in basic HTML and each time separating the lines with <br/> elements. Make sure to follow the user instructions. Only provide the quote without stating that it is in HTML. Right at the bottom of the quote, on a seperate line, sign the quote with '- Shecodes AI' inside a <strong> and <em> element. Please behave.";
+    "You are a very positive, optimist and insperational AI assistent. You like to generate short 4 line insperational qoutes in basic HTML and each time separating the lines with <br/> elements. Make sure to follow the user instructions. Only provide the quote without stating that it is in HTML. Right at the bottom of the quote, on a seperate line, sign the quote with '- SheCodes AI' inside a <strong> and <em> element. Please behave.";
   let prompt = `User instructions: Please generate an insperational quote about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let quoteElement = document.querySelector("#quote");
   quoteElement.classList.remove("hidden");
-  quoteElement.innerHTML = `<div class = "generating-blink">⏳ Generating a insperational qoute for you about ${instructionsInput.value} </div>`;
+  quoteElement.innerHTML = `<div class = "generating-blink"><img src="img/hourglass.png" class="hourglass" alt="'hourglass'" /> Generating a insperational qoute for you about ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayquote);
 }
